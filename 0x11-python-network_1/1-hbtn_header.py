3aa64ab33de3d26f4d1df138c3138210e4af9693
Lloyd022 <hendrickmalatji017@gmail.com>
2023-01-22 11:46:44 +0200

    1-hbtn commit

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-# Task 1. Response header value #0
-from sys import argv
+# Python script that takes in a URL sends a request to the URL
+import sys
 import urllib.request
+
 if __name__ == "__main__":
-    with urllib.request.urlopen(argv[1]) as response:
-        html = response.info()
-        print('{}'.format(html.get('X-Request-Id')))
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        print(res.info()['X-Request-Id'])
